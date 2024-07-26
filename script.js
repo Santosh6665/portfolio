8c1bdda60a7e401e0c6af229819a86b86aa62d83
Santosh Chaudhary <158604567+Santosh6665@users.noreply.github.com>
2024-07-26 15:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+
+
+
+function readDescription(){
+    const target=document.getElementById('targetDescription');
+    const button = document.getElementById('descriptionButton')
+    console.log(button.innerText)
+
+    if(button.innerText=='Read More')
+    {
+        target.classList=['showDescription']
+        button.innerText='Read Less'
+    }
+    else{
+        console.log('Running')
+        target.classList=['hideDescription']
+        button.innerText='Read More'; 
+    }
+
+}
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+}
